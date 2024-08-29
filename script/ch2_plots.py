ea4456c15e39db219086414f9851c377a51500b7
F-Depi <fdp.federico@gmail.com>
2024-08-29 22:14:48 +0200

    animation

--- script/ch2_plots.py
+++ script/ch2_plots.py
@@ -1,5 +1,6 @@
 # ::setlocal makeprg=cd\ script\ &&\ python\ ch2_plots.py
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 import sys
 
@@ -35,7 +36,7 @@ def plot_orbit(l, E):
     tau = data[:, 0]
     r = data[:, 1]
     phi = data[:, 2]
-    theta = data[:, 3]
+    t = data[:, 3]
 
     r_s = np.linspace(0, 2 * np.pi, 100)
 
@@ -71,11 +72,67 @@ def precession(l, E):
     plt.show()
 
 
+def animate_orbit(l, E):
+    if l >= 0:
+        filename = f'data/l{l:.3f}_E{E:.5f}.csv'
+    else:
+        filename = 'data/orbit.csv'
+
+    data = np.loadtxt(filename, delimiter=',', skiprows=1)
+    tau = data[:, 0]
+    r = data[:, 1]
+    phi = data[:, 2]
+
+    r_s = np.linspace(0, 2 * np.pi, 100)
+
+    # Calculate x and y coordinates
+    x = r * np.cos(phi)
+    y = r * np.sin(phi)
+
+    # Create the figure and axis
+    fig, ax = plt.subplots()
+
+    # Initialize the plot with the first point of the trajectory
+    scat = ax.scatter(x[0], y[0], c="b", s=10, label='Current Position')
+    trajectory, = ax.plot([], [], c="r", lw=1, label='Trajectory')  # Line for trajectory
+    plt.plot([0], [0], 'ko', label='black hole')
+    plt.plot(np.cos(r_s), np.sin(r_s), 'k--', label='Event Horizon')
+
+    # Set axis labels
+    ax.set(xlabel=r'$\frac{x}{r_s}$', ylabel=r'$\frac{y}{r_s}$')
+    ax.legend()
+
+    # Set the axis limits if needed
+    ax.set_xlim(min(x) - 1, max(x) + 1)
+    ax.set_ylim(min(y) - 1, max(y) + 1)
+
+    # Update function for animation
+    def update(frame):
+        # Update the scatter plot with the current position
+        scat.set_offsets([x[frame], y[frame]])
+        
+        # Update the trajectory line with all points up to the current frame
+        trajectory.set_data(x[:frame+1], y[:frame+1])
+        
+        return scat, trajectory
+
+    # Create animation
+    ani = animation.FuncAnimation(fig, update, frames=len(tau), interval=1, blit=True)
+
+    # Show the plot
+    plt.show()
+
+    filename = 'media/fall.mp4'
+    ani.save(filename, dpi=400)
+
+
+
 #plot_potential()
-l = float(sys.argv[1])
-E = float(sys.argv[2])
-plot_orbit(l, E)
-precession(l, E)
+#l = float(sys.argv[1])
+#E = float(sys.argv[2])
+#plot_orbit(l, E)
+#precession(l, E)
+animate_orbit(-1, 0)
 
 
 
